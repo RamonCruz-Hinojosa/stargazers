5ad7aa3e8c6fe1925e9d11eff2f0190b4ac71033
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2021-11-30 11:32:38 -0600

    setting up api fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import axios from "axios";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        "api.openweathermap.org/data/2.5/weather?zip=78736,us&appid=3772097231055e1aad10b35fd81157ab"
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
+
   return (
     <div className="bigrid">
       <textarea
