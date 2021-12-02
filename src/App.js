98d2150371b4b66b6aec1b8a863c70e47c43d976
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2021-12-01 20:06:54 -0600

    left notes on how to hide api key

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import "./App.css";
 
+const KEY = process.env.REACT_APP_API_KEY;
+
 function App() {
   useEffect(() => {
     axios
       .get(
-        "http://api.weatherapi.com/v1/current.json?key=3e4f925442a1460797d180312213011&q=Austin&aqi=no"
+        `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=Austin&aqi=no`
       )
       .then((res) => {
         console.log(res);
       });
   }, []);
+  // use env file to hide api key, make function to make li items pop up as fake comment posts are fetched from fake data base, make the comment post a sign in only feature, figure out how to
+  // make requests for any city. the database side and sql need to be done carefully so i dont get hacked
 
   return (
     <div className="bigrid">
